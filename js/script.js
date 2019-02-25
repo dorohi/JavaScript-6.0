162abaaeec70033e1f9cb68f4cdbcbd7b77f94b5
dorohi <m.m.dorohi@gmail.com>
2019-02-25 20:42:47 +0200

    First Lessons

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let money, time;
+money = prompt("Ваш бюджет на месяц?", "");
+time = prompt("Введите дату в формате YYYY-MM-DD", "");
+let mandatoryExpensesFirst = prompt("Введите обязательную статью расходов в этом месяце", "");
+let mandatoryExpensesCostFirst = prompt("Во сколько обойдется?", "");
+let mandatoryExpensesFirstSecond = prompt("Введите обязательную статью расходов в этом месяце", "");
+let mandatoryExpensesCostFirstSecond = prompt("Во сколько обойдется?", "");
+
+let appData = {
+	budget: money,
+	timeData: time,
+	expenses: {
+		mandatoryExpensesFirst: mandatoryExpensesCostFirst,
+		mandatoryExpensesFirstSecond: mandatoryExpensesCostFirstSecond
+	},
+	optionalExpenses:{},
+	income: [],
+	savings: false
+};
+document.querySelector('#oneDayBudget').textContent = appData.budget / 30;
