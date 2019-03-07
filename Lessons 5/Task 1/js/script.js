3afee049d0b4f21c1697082f8d0ab4c0c9d12c5a
dorohi <m.m.dorohi@gmail.com>
2019-03-07 18:20:41 +0200

    5 урок 1 задание

---
+++ Lessons 5/Task 1/js/script.js
@@ -0,0 +1,25 @@
+//Восстановить порядок в меню, добавить пятый пункт
+
+const menu = document.querySelector(".menu"),
+		li = document.createElement('li');
+li.classList.add('menu-item');
+li.textContent = 'Пьятый пункт';
+menu.appendChild(li);
+
+//Заменить картинку заднего фона на другую из папки img
+document.body.style.backgroundImage = 'url(img/apple_true.jpg)';
+
+//  Поменять заголовок, добавить слово "подлинную" ( Получится - "Мы продаем только подлинную технику Apple")
+const title = document.querySelector(".title");
+title.textContent = 'Мы продаем только подлинную технику Apple';
+
+// Удалить рекламу со страницы
+const column = document.querySelectorAll('.column');
+const adv = document.querySelector(".adv");
+//adv.remove(); Не поддерживается IE11-
+column[1].removeChild(adv);
+
+//Спросить у пользователя отношение к технике apple и записать ответ в блок на странице с id "prompt"
+let question = prompt('Как вы относитесь к технике Apple?', '');
+const prom = document.querySelector('#prompt');
+prom.innerHTML = '<h2>' + question + '</h2>';
